b0320f709118e3067b230bd61d1242c7d10efa6c
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-09-14 20:58:27 +0800

    handle errors

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -6,6 +6,7 @@
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
       <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found. Start adding some
         survey results first.</p>
       <ul v-else-if="!isLoading && results?.length">
@@ -27,17 +28,23 @@ export default {
     return {
       results: [],
       isLoading: false,
+      error: null
     }
   },
   methods: {
     async loadExperiences() {
       this.isLoading = true;
+      this.error = null;
+      this.results = [];
       const response = await fetch(
         `${process.env.VUE_APP_FIREBASE_URL}/surveys.json`, {
         headers: {
           'Content-Type': 'application/json'
         },
-      }).then(res => res.json());
+      }).then(res => res.json())
+        .catch(() => {
+          this.error = 'Failed to fetch data - please try again later.';
+        });
       for (const id in response) {
         console.log('id:', id);
         this.results.push({
