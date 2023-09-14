463df8c1e428fd452ee4389e6496c21a2c7c28e5
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-09-14 20:41:40 +0800

    getting data from firebase

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,15 +3,11 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <ul>
-        <survey-result
-          v-for="result in results"
-          :key="result.id"
-          :name="result.name"
-          :rating="result.rating"
-        ></survey-result>
+        <survey-result v-for="result in results" :key="result.id" :name="result.name"
+          :rating="result.rating"></survey-result>
       </ul>
     </base-card>
   </section>
@@ -21,10 +17,33 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
   },
+  data() {
+    return {
+      results: []
+    }
+  },
+  methods: {
+    async loadExperiences() {
+      const response = await fetch(
+        `${process.env.VUE_APP_FIREBASE_URL}/surveys.json`, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then(res => res.json());
+      for (const id in response) {
+        console.log('id:', id);
+        this.results.push({
+          id: id,
+          name: response[id].name,
+          rating: response[id].rating
+        })
+      }
+      console.log(this.results)
+    }
+  }
 };
 </script>
 
